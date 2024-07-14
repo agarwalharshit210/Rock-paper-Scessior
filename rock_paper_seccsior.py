68da238aafca1aa5e53b711fe1a6dcf9133f6a2d
agarwalharshit210 <agarwalharshit2104@gmail.com>
2024-07-14 12:55:52 +0530

    Update rock_paper_seccsior.py

--- rock_paper_seccsior.py
+++ rock_paper_seccsior.py
@@ -1,50 +1,73 @@
 import random
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+def Game():
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+    while True:
+        guess_name = ["rock","paper","scissors"]
+        user_choice = int(input("Enter choice \n0. Rock\n1.Paper\n2.Scissors\n"))
 
-scissors = '''
-    _______
----'   ____)____                             
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+        computer_choice = random.randint(0,2)
 
-guess_name = [rock,paper,scissors]
-user_choice = int(input("Enter choice \n0. Rock\n1.Paper\n2.Scissors\n"))
-print(guess_name[user_choice])
+        if user_choice>=3 or user_choice<0:
+            print("enter valid number")
+            userin = input("You want to play more (Yes/No) ")
+            if userin.lower() == 'yes':
+                pass
+            else:
+              print("Thank you")
+              break 
+        elif user_choice == 0 and computer_choice==2:
+            print(f"You choice : {guess_name[user_choice]}")
+            print(f"Computer choice : {guess_name[computer_choice]}")
+            print("You Win")
+            userin = input("You want to play more (Yes/No) ")
+            if userin.lower() == 'yes':
+                pass
+            else:
+              print("Thank you")
+              break 
+        elif computer_choice==0 and user_choice==2:
+            print(f"You choice : {guess_name[user_choice]}")
+            print(f"Computer choice : {guess_name[computer_choice]}")
+            print("You Lose") 
+            userin = input("You want to play more (Yes/No) ")
+            if userin.lower() == 'yes':
+                pass
+            else:
+              print("Thank you")
+              break   
+        elif user_choice>computer_choice:
+            print(f"You choice : {guess_name[user_choice]}")
+            print(f"Computer choice : {guess_name[computer_choice]}")
+            print("You Win")  
+            userin = input("You want to play more (Yes/No) ")
+            if userin.lower() == 'yes':
+                pass
+            else:
+               print("Thank you")
+               break  
+        elif computer_choice>user_choice:
+            print(f"You choice : {guess_name[user_choice]}")
+            print(f"Computer choice : {guess_name[computer_choice]}")
+            print("You Lose")
+            userin = input("You want to play more (Yes/No) ")
+            if userin.lower() == 'yes':
+                pass
+            else:
+               print("Thank you")
+               break   
+        elif computer_choice == user_choice:
+            print(f"You choice : {guess_name[user_choice]}")
+            print(f"Computer choice : {guess_name[computer_choice]}")
+            print("Draw")  
+            userin = input("You want to play more (Yes/No) ")
+            if userin.lower() == 'yes':
+                pass
+            else:
+               print("Thank you")
+               break       
 
-computer_choice = random.randint(0,2)
-print(guess_name[computer_choice])
+        print()    
 
-if user_choice>=3 and user_choice<0:
-    print("enter valid number")
-elif user_choice == 0 and computer_choice==2:
-    print(computer_choice,user_choice,"You Win")
-elif computer_choice==0 and user_choice==2:
-    print(computer_choice,user_choice,"You Lose")   
-elif user_choice>computer_choice:
-    print(computer_choice,user_choice,"You Win")   
-elif computer_choice>user_choice:
-    print(computer_choice,user_choice,"You Lose")
-elif computer_choice == user_choice:
-    print(computer_choice,user_choice,"Draw")    
-
-print()    
+if __name__ == '__main__':
+    Game()
